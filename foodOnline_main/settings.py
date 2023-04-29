72d5100c758e1dadad3676a03750e1f0405ed90f
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-29 13:05:00 +0530

    add the static files

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -115,9 +115,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# static files configuration
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'foodOnline_main/static'
+]
