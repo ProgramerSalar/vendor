2e3912975603d667aa31ac5715a3e7b098d3881c
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-29 15:22:31 +0530

    password hide

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,13 +4,17 @@ from django.contrib.auth.admin import UserAdmin
 
 
 # Register your models here.
-# class CustomUserAdmin(UserAdmin):
-#     list_display = ('email' , 'first_name' , 'last_name' , 'username', 'role' , 'is_active')
-#     ordering = ('-date_joined',)
-#     filter_horizontal = ()
-#     list_filter = ()
-#     fieldsets = ()
+class CustomUserAdmin(UserAdmin):
+    
+    # show the that in panel
+    list_display = ('email' , 'first_name' , 'last_name' , 'username', 'role' , 'is_active')
+    ordering = ('-date_joined',)
+    
+    # password encrypt
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
 
 
 
-admin.site.register( User)
+admin.site.register( User , CustomUserAdmin)
