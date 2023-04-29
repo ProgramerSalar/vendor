fed7489798679579ee4f0e464549e3d1d7efa620
ProgramerSalar <manishkumar60708090@gmail.com>
2023-04-29 15:12:28 +0530

    user model

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -83,6 +84,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
